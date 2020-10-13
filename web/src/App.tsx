8c648a82752dbbb37ba792e2ae1f8ac9306d9ae0
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-10-12 21:57:08 -0300

    Criando CSS Global

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './styles/global.css';
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello Word</h1>
+        <h1>Hello Word teste2</h1>
     </div>
   );
 }
