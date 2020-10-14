b43d3213b42e45123129258a8ae89b084a694283
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-10-13 22:14:15 -0300

    Criado Conexao com o Banco

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
+import "./database/connection";
+
 const app = express();
 
 app.get("/users", (request, response) => {
-  return response.json({message: 'Hello Word'})
+  return response.json({ message: "Hello Word" });
 });
 
 app.listen(3333);
