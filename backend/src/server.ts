180f5517756194345f4ab1d0ca7e5180817f8d3d
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-10-16 23:16:00 -0300

    Criando Orfanatos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,39 @@
 import express from "express";
+import { getRepository } from "typeorm";
+import Orphanage from "./models/Orphanage";
 
 import "./database/connection.ts";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  return response.json({ message: "Hello Word" });
+app.use(express.json());
+
+app.post("/orphanages", async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  const orphanageRepository = getRepository(Orphanage);
+
+  const orphanage = orphanageRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanageRepository.save(orphanage);
+
+  return response.status(201).json(orphanage);
 });
 
 app.listen(3333);
