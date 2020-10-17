518e3a7a654718717848e75a5c3dac34b54197df
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-10-17 20:00:41 -0300

    Barra de Busca Orfanatos

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
 
 import mapMarker from './src/images/map-marker.png';
 
@@ -30,7 +31,13 @@ export default function App() {
           </Callout>
         </Marker>
       </MapView>
-    </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>{"2 Orfanatos encontrados"}</Text>
+        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { }} >
+          <Feather name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </View >
   );
 }
 
@@ -54,5 +61,34 @@ const styles = StyleSheet.create({
   calloutText: {
     color: '#0089a5',
     fontSize: 14,
-  }
+  },
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 23,
+    bottom: 56,
+
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    elevation: 3,
+  },
+  footerText: {
+    color: '#8fa7b3',
+  },
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
