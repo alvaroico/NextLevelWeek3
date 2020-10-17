8d348b29853fcc031224276040c7473639328d8b
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-10-17 01:13:05 -0300

    Tratamento de erro no cadastro

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import path from "path";
+import "express-async-errors";
 import "./database/connection.ts";
 
 import routes from "./routes";
+import errorHandler from "./errors/handles";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(errorHandler);
 
 app.listen(3333);
