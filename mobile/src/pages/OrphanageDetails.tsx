a42e618e65f8f185b770dba78c58c08131321613
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-10-18 13:37:32 -0300

    Destalhe do Orfanato com Rotas

--- mobile/src/pages/OrphanageDetails.tsx
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -6,12 +6,13 @@ import {
   Text,
   StyleSheet,
   Dimensions,
+  Linking,
 } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { Feather, FontAwesome } from "@expo/vector-icons";
 
 import mapMarkerImg from "../images/map-marker.png";
-import { RectButton } from "react-native-gesture-handler";
+import { RectButton, TouchableOpacity } from "react-native-gesture-handler";
 import { useRoute } from "@react-navigation/native";
 import api from "../services/api";
 
@@ -54,6 +55,9 @@ export default function OrphanageDetails() {
     );
   }
 
+  function handleOpenGoogleMapRoutes(){
+    Linking.openURL(`https://www.google.com/maps/dir/?api=1&destination=${orphanage?.latitude},${orphanage?.longitude}`)
+  }
   return (
     <ScrollView style={styles.container}>
       <View style={styles.imagesContainer}>
@@ -97,9 +101,9 @@ export default function OrphanageDetails() {
             />
           </MapView>
 
-          <View style={styles.routesContainer}>
+          <TouchableOpacity onPress={handleOpenGoogleMapRoutes} style={styles.routesContainer}>
             <Text style={styles.routesText}>Ver rotas no Google Maps</Text>
-          </View>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.separator} />
