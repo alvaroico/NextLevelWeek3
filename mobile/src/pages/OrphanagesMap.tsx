29ca7acd6c0924c56164279764fb65804764faf7
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-10-17 21:39:56 -0300

    Criado Rotas Dos Orfanatos Detalhes e criação

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -12,6 +12,9 @@ export default function OrphanagesMap() {
   function handleNavigateToOrphanageDetails() {
     navigation.navigate('OrphanageDetails')
   }
+  function handleNavigateToCreateOrphanage() {
+    navigation.navigate('SelectMapPosition')
+  }
 
 
   return (
@@ -41,7 +44,7 @@ export default function OrphanagesMap() {
       </MapView>
       <View style={styles.footer}>
         <Text style={styles.footerText}>{"2 Orfanatos encontrados"}</Text>
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { }} >
+        <TouchableOpacity style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage} >
           <Feather name="plus" size={20} color="#FFF" />
         </TouchableOpacity>
       </View>
