cec2a1f8337d505eb416fc05bf486de594bdc2cc
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-10-18 15:56:07 -0300

    Cadastro Funcional

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -4,7 +4,7 @@ import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { Feather } from "@expo/vector-icons";
 
 import mapMarker from "../images/map-marker.png";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
 import api from "../services/api";
 
@@ -19,11 +19,11 @@ export default function OrphanagesMap() {
   const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
   const navigation = useNavigation();
 
-  useEffect(() => {
+  useFocusEffect(() => {
     api.get("orphanages").then((response) => {
       setOrphanages(response.data);
     });
-  }, []);
+  });
 
   function handleNavigateToOrphanageDetails(id: number) {
     navigation.navigate("OrphanageDetails", { id });
@@ -71,7 +71,9 @@ export default function OrphanagesMap() {
         })}
       </MapView>
       <View style={styles.footer}>
-        <Text style={styles.footerText}>{`${orphanages.length} Orfanatos encontrados`}</Text>
+        <Text
+          style={styles.footerText}
+        >{`${orphanages.length} Orfanatos encontrados`}</Text>
         <RectButton
           style={styles.createOrphanageButton}
           onPress={handleNavigateToCreateOrphanage}
